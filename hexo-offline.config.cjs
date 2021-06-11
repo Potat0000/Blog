33f1e285fbd9b1847bc6eced58e60f5175c2092a
Potat0 <i@potat0.cc>
2021-06-11 18:42:07 +0800

    切换到阿里云OSS

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -1,21 +1,23 @@
 module.exports = {
-    globDirectory: ".github/workflows",
+    globPatterns: ["**/*.{html,js,css,webp,png,jpg,gif,svg,eot,ttf,woff,woff2}"],
+    globIgnores: ["img/avatar.png", "img/default.png", "img/favicon.png", "lib/hint/hint.min.css"],
+    globDirectory: "./public",
     runtimeCaching: [
-        {
-            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/,
-            handler: "CacheFirst"
-        },
         {
             urlPattern: /^https:\/\/at\.alicdn\.cn\/.*/,
             handler: "CacheFirst"
         },
         {
-            urlPattern: /^https:\/\/utteranc\.es\/.*/,
+            urlPattern: /^https:\/\/www\.google-analytics\.com\/.*/,
+            handler: "NetworkFirst"
+        },
+        {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/,
             handler: "CacheFirst"
         },
         {
-            urlPattern: /^https:\/\/www\.google-analytics\.com\/.*/,
-            handler: "NetworkFirst"
+            urlPattern: /^https:\/\/utteranc\.es\/.*/,
+            handler: "CacheFirst"
         }
     ]
 }
