d60e5cb5a8ee4328155454894c4e1d417ea250e9
Potat0 <i@potat0.cc>
2021-10-10 10:53:17 +0800

    启用新特性

--- build_script/get_libs.sh
+++ build_script/get_libs.sh
@@ -4,6 +4,8 @@ curl https://cdn.jsdelivr.net/npm/anchor-js@4/anchor.min.js --create-dirs -o anc
 curl https://cdn.jsdelivr.net/npm/bootstrap@4/dist/css/bootstrap.min.css --create-dirs -o bootstrap/css/bootstrap.min.css
 curl https://cdn.jsdelivr.net/npm/bootstrap@4/dist/js/bootstrap.min.js --create-dirs -o bootstrap/js/bootstrap.min.js
 curl https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js --create-dirs -o clipboard/clipboard.min.js
+curl https://cdn.jsdelivr.net/npm/echarts@5/dist/echarts.min.js --create-dirs -o echarts/echarts.min.js
+curl https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/zh/asset/theme/macarons.min.js --create-dirs -o echarts/macarons.min.js
 curl https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3/dist/jquery.fancybox.min.css --create-dirs -o fancybox/jquery.fancybox.min.css
 curl https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3/dist/jquery.fancybox.min.js --create-dirs -o fancybox/jquery.fancybox.min.js
 curl https://cdn.jsdelivr.net/npm/github-markdown-css@4/github-markdown.min.css --create-dirs -o github-markdown-css/github-markdown.min.css
