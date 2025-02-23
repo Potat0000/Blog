e0c116c39383d3bdbb086986fb11762425796677
Potat0 <i@potat0.cc>
2025-02-23 21:28:21 +0800

    升级依赖

--- build_script/gen_updatedtime.py
+++ build_script/gen_updatedtime.py
@@ -3,7 +3,7 @@
 import os
 import re
 
-os.chdir("source/_posts")
+os.chdir('source/_posts')
 files = [x for x in list(os.walk('.'))[0][2] if re.match(r'^\d{4}-\d{2}-\d{2}-.+?\.md$', x)]
 for file in files:
     createDate = file[0:10]
